2a72f5529f270f9984ded2ff0acd0ca3341fdee4
Conrad Thomas <thomcs4@student.op.ac.nz>
2020-06-15 18:01:23 +1200

    Added answer checking with dialog box for info

--- app/src/main/java/thomcs4/bit/QuestionActivity.java
+++ app/src/main/java/thomcs4/bit/QuestionActivity.java
@@ -4,11 +4,14 @@ import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.DialogInterface;
+import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import org.w3c.dom.Text;
 
@@ -16,6 +19,7 @@ public class QuestionActivity extends AppCompatActivity implements View.OnClickL
 
     private Questions questions = new Questions();
     private int qIndex = 0;
+    private int score = 0;
     private Button option1;
     private Button option2;
     private Button option3;
@@ -38,10 +42,14 @@ public class QuestionActivity extends AppCompatActivity implements View.OnClickL
         questionInfo = (TextView) findViewById(R.id.questionTitle);
         picture = (ImageView) findViewById(R.id.questionPicture);
         option1 = (Button) findViewById(R.id.o1);
+        option1.setOnClickListener(this);
         option2 = (Button) findViewById(R.id.o2);
+        option2.setOnClickListener(this);
         option3 = (Button) findViewById(R.id.o3);
+        option3.setOnClickListener(this);
         option4 = (Button) findViewById(R.id.o4);
-        answerDesc = new AlertDialog.Builder(this);
+        option4.setOnClickListener(this);
+        answerDesc = new AlertDialog.Builder(this).setCancelable(false);
         loadQuestion();
     }
 
@@ -54,8 +62,10 @@ public class QuestionActivity extends AppCompatActivity implements View.OnClickL
             option4.setText(questions.getOption(qIndex, 4));
             answer = questions.getAnswer(qIndex);
             picture.setImageResource(questions.getImage(qIndex));
+        } else {
+            Intent changeActivity = new Intent(QuestionActivity.this, ResultsActivity.class);
+            startActivity(changeActivity);
 
-            qIndex++;
         }
 
     }
@@ -66,18 +76,41 @@ public class QuestionActivity extends AppCompatActivity implements View.OnClickL
         Button clicked = (Button) view;
 
         //creates a pop up message with answer.
+
+        if (clicked.getText().equals(answer)){
+            clicked.setBackgroundColor(Color.GREEN);
+            showDesc();
+            score++;
+            clicked.setBackgroundColor(Color.parseColor("#35e8aa"));
+        }
+        else {
+            clicked.setBackgroundColor(Color.parseColor("#fc496a"));
+        }
+
+    }
+
+
+    private void showDesc(){
         answerDesc.setMessage(questions.getAnswerDesc(qIndex))
-                .setTitle("Answer : " + answer)
-                .setPositiveButton("Continue", new DialogInterface.OnClickListener() {
+                .setTitle("Answer: " + answer)
+                .setPositiveButton("Next", new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
                         dialog.dismiss();
+                        clearButtons();
+                        qIndex++;
+                        loadQuestion();
                     }
                 })
                 .create();
         answerDesc.show();
+    }
 
-
+    private void clearButtons() {
+        option1.setBackgroundColor(Color.parseColor("#F0F2F1"));
+        option2.setBackgroundColor(Color.parseColor("#F0F2F1"));
+        option3.setBackgroundColor(Color.parseColor("#F0F2F1"));
+        option4.setBackgroundColor(Color.parseColor("#F0F2F1"));
     }
 
 }
